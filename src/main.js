72530b2175dfacff97f2716032c053bc3b4519ad
Sanjana-Lama23 <sl2005sana@gmail.com>
2026-06-18 10:39:02 +0530

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".text",{
+     strings: [
+          "Android Developer",
+          "Kotlin Learner",
+          "B.tech CSE student"
+         ],
+     typeSpeed: 100,
+     backSpeed: 80,
+     backDelay: 1200,
+     loop: true
+});
